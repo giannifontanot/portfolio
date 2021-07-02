fdf8cf67bc19d652fae7ee05593212025c08e1e6
giannifontanot <giannifontanot@gmail.com>
2021-07-02 08:11:51 -0500

    Fix error on menu. Created function openHTML to open local pages and openLIVE to open external URLs.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,3 +7,7 @@ function fOpenLive(pPage) {
     window.open('https://giannifontanot.github.io/' + pPage, '_blank',);
 }
 
+function fOpenHTML(pPage) {
+    window.open('./' + pPage, '_blank',);
+}
+
